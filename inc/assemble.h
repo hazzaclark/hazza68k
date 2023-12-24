6d529e8ab01b0825d7d235439c17638cd5337831
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-12-24 18:41:08 +0000

    added line semantic methods

--- inc/assemble.h
+++ inc/assemble.h
@@ -30,6 +30,11 @@ static INSTRUCTION* TYPE ## VALUE(int, char*, char*, int*)
 #define         DIRECTIVE_CHARS         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?_"
 #define         LABEL_CHARS             DIRECTIVE_CHARS ".@"
 
+
+#define         PARAM_FALSE_IF                0
+#define         PARAM_TOTAL                   1
+#define         PARAM_SUFFIX                  ''
+
 typedef struct ASSEMBLER
 {
     U32* GPR;
@@ -51,19 +56,41 @@ typedef struct FILE_SEMANTIC
     char* WRITE_BUFFER;
     char* LINE_BUFFER[0];
     char* SOURCE_LINE;
-    INSTRUCTION* INSTR_BASE;
+    struct INSTRUCTION* INSTR_BASE;
     S32* LINE_CHAR;
+
+    FILE_MODE MODE;
 
 } FILE_SEMANTIC;
 
 typedef struct LINE_SEMNATIC
 {
-    
+    struct LINE_SEMNATIC* NEXT_LINE;
+    unsigned int HEAD;
+    unsigned int TAIL:1;
+    char* SOURCE;
 
 } LINE_SEMNATIC;
 
+typedef enum FILE_MODE
+{
+    MODE_NORMAL,
+    MODE_REPEAT,
+    MODE_MACRO,
+    
+} FILE_MODE;
+
+typedef enum SYMBOL_MODE
+{
+    SYMBOL_CONST,
+    SYMBOL_VAR,
+    SYMBOL_MACRO,
+    SYMBOL_LABEL,
+
+} SYMBOL_MODE;
+
 void ASSEMBLE_FILE(FILE_SEMANTIC* FILE_STATE, FILE* INPUT, ASSEMBLER* ASSEMBLER);
-void ASSEMBLE_LINE(FILE_SEMANTIC* FILE_STATE, const char* SOURCE);
+void ASSEMBLE_LINE(FILE_SEMANTIC* FILE_STATE, char* SOURCE);
 
 #endif
 #endif

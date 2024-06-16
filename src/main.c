eb58775ac108fa1aa70f8819a8b87b2c3ee31741
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-16 22:27:52 +0100

    added case and error checks

--- src/main.c
+++ src/main.c
@@ -14,11 +14,36 @@
 int main(int argc, char** argv)
 {
     bool PRINT_USAGE;
+    char* INPUT_FILE_PATH;
+    char* OUTPUT_FILE_PATH;
+    char* LISTING_FILE_PATH;
+    char* SYMBOL_FILE_PATH;
+
+    bool CASE_INSENSITIVE;
+    bool ADDRESS_ADD;
+    bool WARNINGS;
+
+    int INDEX;
 
     /* WE CHECK TO SEE IF THE CODE CONDITION OF THE PROGRAM */
     /* IS MET BEFORE DISPLAYING CONTENTS */
 
     /* AFTER WHICH, WE PRINT CONTENTS TO THE SCREEN */
+
+    for(INDEX = 1; INDEX < argc; INDEX++)
+    {
+        if(argv[INDEX][1] = '-')
+        {
+            switch (argv[INDEX][1])
+            {
+                case 'i':
+                    INPUT_FILE_PATH = argv[INDEX];
+                    INDEX++;
+                    continue;
+
+            }
+        }
+    }
 
     if(argc < 2 || PRINT_USAGE)
     {
@@ -26,9 +51,25 @@ int main(int argc, char** argv)
         (
             "-----------------------------------------\n"
             "Motorola 68000 Assembler - by Harry Clark\n"
+            "\n"
+            "Options:                                 \n"
+            " -i [path] - Input File.                 \n"
+            " -o [path] - Output File.                \n"
+            " -l [path] - Listing File.               \n"
             "-----------------------------------------\n"
         , stdout);
     }
+
+    else
+    {
+        if(OUTPUT_FILE_PATH == NULL)
+        {
+            fprintf(stderr, "Output File path must be specified with '-o'.");
+            return EXIT_FAILURE;
+        }
+    }
+
+    return 0;
 }
 
 #endif

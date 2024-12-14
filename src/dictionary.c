0376d5a98e7b2700141be24ce3f72f8c771d1d7e
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2024-12-14 13:39:23 +0000

    Update dictionary.c

--- src/dictionary.c
+++ src/dictionary.c
@@ -12,6 +12,10 @@ static OUTPUT* OUTPUT_API;
 static FILE* OUTPUT_FILE = NULL;
 static int OPTION_FLAG = OPTION_NONE;
 
+//=================================================
+//              DICTIONARY HANDLERS
+//=================================================
+
 void NEXT_LINE(int LINE, char* SOURCE)
 {
     OUTPUT_API->NEXT_LINE(LINE, SOURCE);
@@ -40,4 +44,42 @@ void END_OUTPUT(void)
         fclose(OUTPUT_FILE);
         OUTPUT_FILE = NULL;
     }
+}
+
+//=================================================
+//          MISC. FUNCTIONS AND HANDLERS
+//=================================================
+
+/* LOOK THROUGH THE DESIGNATED FILE PROVIDED TO LOOK FOR IDENTIFIER'S */
+/* SUCH AS NAME USED FOR DEFINING MACROS, HEADERS, ETC */
+
+IDENTIFIER* LOCATE_IDEN(char* VALUE)
+{
+    IDENTIFIER* LOOK = NULL;
+    IDENTIFIER* ADDRESS = NULL;
+    int INDEX = 0;
+
+    /* ASSERT IT'S TRUTH VALUE BEFORE READING */
+
+    assert(VALUE != NULL);
+
+    /* THIS WILL LOOK TO DETERMINE IF THERE IS A RELEVANT AND CORRESPONDING */
+    /* VALUE WITH A DESIGNATED ADDRESS */
+
+    while((LOOK = &ADDRESS) != NULL)
+    {
+        if((INDEX = strcmp(VALUE, LOOK->NAME)) == 0) return(LOOK);
+    }
+
+    /* IF THE IDENTIFIER IS NOT ACCESSED YET, WE CAN ACCESS IT NOW */
+
+    printf("New Identifier %s\n", VALUE);
+
+    LOOK->NAME = strrchr(VALUE, 0);
+    LOOK->DEFINED = false;
+    LOOK->VALUE = 0;
+    LOOK->BEFORE = NULL;
+    LOOK->AFTER = NULL;
+    ADDRESS == LOOK;
+    return LOOK;
 }
